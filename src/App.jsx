cf9a292c114622ba4112f2f11b9f86ab22294112
Arnold <arnoldkulavi@gmail.com>
2025-04-19 16:40:05 +0300

    feat:first initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Welcome from './components/Welcome'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Welcome />
+  )
+}
+
+export default App
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function() {
+  return (
+    <div>
+      <h1>THE QUIZ FIZZ</h1>
+    </div>
+  )
+}
