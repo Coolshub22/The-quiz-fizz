13fe024e001bbc78277bbbd0470313d52f2f53b2
Edwin kipyego <koriredwin95@gmail.com>
2025-04-22 14:49:52 +0300

    feat:add two p tags ans h2

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,6 +4,9 @@ export default function() {
   return (
     <div>
       <h1>THE QUIZ FIZZ</h1>
+      <h2>Welcome to the Quiz Fizz</h2>
+      <p>Test your knowledge with our fun and interactive quizzes!</p>
+      <p>Click the button below to get started.</p>
     </div>
   )
 }
